4e501eeca85a0297823c5b31d05175330e33247f
Nayan Thacker <nayanthacker28@gmail.com>
2025-12-19 11:51:03 +0530

    Created CI/CD workflows for frontend build.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': '/src',
     },
-  },
-   build: {
-    outDir: '../docs'
   }
 })
