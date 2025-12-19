d6047dc26b00f0bbc76b795249615b1d796cd7f9
Nayan Thacker <nayanthacker28@gmail.com>
2025-12-19 14:09:21 +0530

    Change router from BrowserRouter to HashRouter for better compatibility with static file hosting

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate, } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, } from 'react-router-dom';
 import React, { useContext } from 'react';
 import App from '../App';
 import RegistrationForm from '../components/forms/RegistrationForm';
